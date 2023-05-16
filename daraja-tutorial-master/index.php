0b74d1ed262ca835d94d13106d62135f3abe21e7
Nonentity <73635465+collotebik@users.noreply.github.com>
2023-05-16 14:50:47 +0300

    css commit

--- daraja-tutorial-master/index.php
+++ daraja-tutorial-master/index.php
@@ -9,6 +9,8 @@
       href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
       rel="stylesheet"
     />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
     <link href="" rel="stylesheet" />
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" ">
@@ -16,59 +18,6 @@
       type="text/javascript"
       src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
     ></script>
-    <style>
-      @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
-
-      body {
-        background-color: #eaedf4;
-        font-family: "Rubik", sans-serif;
-      }
-
-      .card {
-        width: 310px;
-        border: none;
-        border-radius: 15px;
-      }
-
-      .justify-content-around div {
-        border: none;
-        border-radius: 20px;
-        background: #f3f4f6;
-        padding: 5px 20px 5px;
-        color: #8d9297;
-      }
-
-      .justify-content-around span {
-        font-size: 12px;
-      }
-
-      .justify-content-around div:hover {
-        background: #545ebd;
-        color: #fff;
-        cursor: pointer;
-      }
-
-      .justify-content-around div:nth-child(1) {
-        background: #545ebd;
-        color: #fff;
-      }
-
-      span.mt-0 {
-        color: #8d9297;
-        font-size: 12px;
-      }
-
-      h6 {
-        font-size: 15px;
-      }
-      .mpesa {
-        background-color: green !important;
-      }
-
-      img {
-        border-radius: 15px;
-      }
-    </style>
   </head>
   <body oncontextmenu="return false" class="snippet-body">
     <div class="container d-flex justify-content-center">
