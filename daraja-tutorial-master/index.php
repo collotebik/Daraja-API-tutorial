425958522355ef0abb1aaf142078d60d9aa983e5
Nonentity <73635465+collotebik@users.noreply.github.com>
2023-05-16 14:58:58 +0300

    Update index.php

--- daraja-tutorial-master/index.php
+++ daraja-tutorial-master/index.php
@@ -17,7 +17,7 @@
     <script
       type="text/javascript"
       src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
-    ></script>
+    </script>
   </head>
   <body oncontextmenu="return false" class="snippet-body">
     <div class="container d-flex justify-content-center">
