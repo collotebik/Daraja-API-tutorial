816547855c37f731ee0cd6fa2916704d099de752
Nonentity <73635465+collotebik@users.noreply.github.com>
2023-05-16 14:59:25 +0300

    Update index.php

--- daraja-tutorial-master/index.php
+++ daraja-tutorial-master/index.php
@@ -16,7 +16,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" ">
     <script
       type="text/javascript"
-      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
     </script>
   </head>
   <body oncontextmenu="return false" class="snippet-body">
